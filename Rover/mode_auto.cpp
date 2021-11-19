453c669bf969b19ea5198c18cd7e799ee703ea6a
JackSun9 <jacksun9899@gmail.com>
2021-11-18 22:52:09 -0800

    add mode_auto call golf_start_mission

--- Rover/mode_auto.cpp
+++ Rover/mode_auto.cpp
@@ -5,6 +5,11 @@
 
 bool ModeAuto::_enter()
 {
+    if (rover.start_auto)
+    {
+        rover.golf_start_mission();
+    }
+
     // fail to enter auto if no mission commands
     if (mission.num_commands() <= 1) {
         gcs().send_text(MAV_SEVERITY_NOTICE, "No Mission. Can't set AUTO.");
